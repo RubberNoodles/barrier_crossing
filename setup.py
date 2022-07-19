c854c88c4a98a978de3701c4c2f18aaf238f9534
RubberNoodles <ocheng@ualberta.ca>
2022-07-19 13:58:01 -0400

    Setup.py for distribution.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name = "barrier_crossing",
+  version = "0.0.1",
+  install_requires = [
+    "numpy",
+    "matplotlib",
+    "scipy",
+    "tensorflow_probability",
+    "tqdm",
+    "typing",
+    "pandas",
+    "jaxlib", 
+    "jax",
+    "jraph",
+    "jax-md"
+  ]
+)
