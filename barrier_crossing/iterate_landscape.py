0f592b076dbe959dae45462e3f2ec57999e79cfb
zosia01 <66381368+zosia01@users.noreply.github.com>
2022-08-02 15:41:49 -0400

    Update iterate_landscape.py

--- barrier_crossing/iterate_landscape.py
+++ barrier_crossing/iterate_landscape.py
@@ -16,7 +16,7 @@ from jax_md import space
 from barrier_crossing.energy import V_biomolecule_geiger, V_biomolecule_reconstructed
 from barrier_crossing.protocol import make_trap_fxn, make_trap_fxn_rev, linear_chebyshev_coefficients
 from barrier_crossing.simulate import simulate_brownian_harmonic, batch_simulate_harmonic
-from barrier_crossing.optimize import estimate_gradient_fwd, optimize_protocol
+from barrier_crossing.optimize import estimate_gradient_fwd, optimize_protocol, find_bin_timesteps
 
 def energy_reconstruction(works, trajectories, bins, trap_fn, simulation_steps, batch_size, k_s, beta):
   """
@@ -143,8 +143,9 @@ def optimize_landscape(ground_truth_energy_fn,
     
     # Optimize a protocol with this new landscape
     logging.info("Optimiziing protocol from linear using reconstructed landscape.")
+    bin_timesteps = find_bin_times(energy_fn_guess, simulate_fn, trap_fn, simulation_steps, key, bins)
     
-    grad_fxn = lambda num_batches: grad_fn_no_E(num_batches, energy_fn_guess)
+    grad_fxn = lambda num_batches: grad_fn_no_E(num_batches, energy_fn_guess, bin_timesteps)
     lin_trap_coeffs = linear_chebyshev_coefficients(r0_init,r0_final,simulation_steps, degree = 12, y_intercept = r0_init)
     coeffs_, _, losses = optimize_protocol(lin_trap_coeffs, grad_fxn, optimizer, batch_size, opt_steps)
     
