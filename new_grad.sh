5190b92c923b154749abc4673bfda9cc20f0900b
zosia01 <66381368+zosia01@users.noreply.github.com>
2022-09-03 14:09:01 +0100

    change runtime

--- new_grad.sh
+++ new_grad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:4 #how many GPUs 
-#SBATCH -t 0-01:00          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p gpu_test   # Partition to submit to
+#SBATCH -t 1-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p seas_dgx1   # Partition to submit to
 #SBATCH --mem-per-cpu=64000   # Memory pool for all cores in MB
 #SBATCH -o test_output.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e test_errors.err  # File to which STDERR will be written, %j inserts jobid
