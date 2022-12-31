0e718f1dd23fc267d84b01fdc9a2289f3810572f
zosia01 <66381368+zosia01@users.noreply.github.com>
2022-12-31 12:57:12 +0100

    Add version of gradient with sample extensions

--- barrier_crossing/optimize.py
+++ barrier_crossing/optimize.py
@@ -232,6 +232,38 @@ def estimate_gradient_acc_rev2(error_samples,batch_size,
     return grad_total, loss, gradient_estimator_total, summary_total
   return _estimate_grad
 
+def estimate_gradient_acc_rev_extensions(error_samples,batch_size,
+                          energy_fn,
+                          init_position, r0_init, r0_final,
+                          Neq, shift,
+                          simulation_steps, dt,
+                          temperature, mass, gamma, beta):
+  """ 
+  New version of estimate_gradient_acc_rev2, which takes
+  samples of extensions instead of time steps in simulation.
+  ToDo: change returns, so it's compatible with optimize_protocol
+
+  """
+  def _estimate_grad(coeffs, seed):
+    grad_total = jnp.zeros(len(coeffs))
+    gradient_estimator_total = []
+    summary_total = []
+    loss = 0.0
+    for r in error_samples:
+      grad_func = estimate_gradient_rev(batch_size,
+                          energy_fn,
+                          init_position, r, r0_final,
+                          Neq, shift,
+                          simulation_steps, dt,
+                          temperature, mass, gamma, beta)
+      grad, (gradient_estimator, summary) = grad_func(coeffs, seed)
+      grad_total += grad
+      gradient_estimator_total.append(gradient_estimator)
+      summary_total.append(summary)
+      loss += summary[2]
+    return grad_total, loss, gradient_estimator_total, summary_total
+  return _estimate_grad
+
 def optimize_protocol(init_coeffs, batch_grad_fn, optimizer, batch_size, num_steps, save_path = None):
   """ Training loop to optimize the coefficients of a chebyshev polynomial that defines the 
   protocol of moving a harmonic trap over a free energy landscape.
