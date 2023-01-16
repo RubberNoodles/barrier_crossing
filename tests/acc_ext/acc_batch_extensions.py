9eba17f073fcb52f3519035c8602f1dfdab0018f
Oliver Cheng <ocheng@holygpu7c0915.rc.fas.harvard.edu>
2023-01-15 22:26:13 -0500

    Auto-commit.

--- tests/acc_ext/acc_batch_extensions.py
+++ tests/acc_ext/acc_batch_extensions.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     beta_sc)
 
   batch_size = 2000 # Number of simulations/trajectories simulated. GPU optimized.
-  opt_steps = 2 # Number of gradient descent steps to take.
+  opt_steps = 400 # Number of gradient descent steps to take.
 
   #lr = jopt.exponential_decay(0.3, opt_steps, 0.003)
   lr = jopt.polynomial_decay(1., opt_steps, 0.001)
--- tests/acc_ext/acc_batch_extensions.sh
+++ tests/acc_ext/acc_batch_extensions.sh
@@ -14,7 +14,7 @@ pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases
 
 num_extension=6
 
-if [${SLURM_ARRAY_TASK_ID}==1 ]; then
+if [ ${SLURM_ARRAY_TASK_ID} -eq 1 ]; then
   if [ -d "./data" ]
   then 
     rm -rf "./data"
@@ -30,7 +30,7 @@ done
 
 python3 acc_batch_extensions.py ${SLURM_ARRAY_TASK_ID}
 
-if [ ${SLURM_ARRAY_TASK_ID}==6 ]; then
+if [ ${SLURM_ARRAY_TASK_ID} -eq 6 ]; then
 	git add .
 	git commit -m "Auto-commit."
 	git push
