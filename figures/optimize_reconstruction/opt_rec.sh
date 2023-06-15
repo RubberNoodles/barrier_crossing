8aa6c670bd595d2c03a93f0dd6e413dcad95eee3
RubberNoodles <ocheng@college.harvard.edu>
2023-06-15 07:02:57 -0400

    Update module imports for FASRC cluster.

--- figures/optimize_reconstruction/opt_rec.sh
+++ figures/optimize_reconstruction/opt_rec.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem-per-cpu=128000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_%A_eps_%a.out  # Requires ./log directory
 #SBATCH -e err_%A_eps_%a.err  # File to which STDERR will be written, %j inserts jobid
-module load python/3.8.5-fasrc01
-module load cuda/11.4.2-fasrc01
-module load cudnn/8.2.2.26_cuda11.4-fasrc01
+module load python/3.10.9-fasrc01
+module load cuda/12.0.1-fasrc01
+module load cudnn/8.8.0.121_cuda12-fasrc01
 pip3 install -e ../../
 pip3 install -r ./../../requirements.txt
-pip3 install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip3 install "jax[cuda12_cudnn88]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 python3 ./opt_rec.py
