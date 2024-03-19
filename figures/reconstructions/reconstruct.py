7d59680b0019c7f278f4ce2797281b90fa50fd70
RubberNoodles <ocheng@college.harvard.edu>
2024-03-19 01:11:52 -0400

    Only plot top x protocols.

--- figures/reconstructions/reconstruct.py
+++ figures/reconstructions/reconstruct.py
@@ -17,16 +17,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # from figures.params import * # global variables;
 
+def plot_reconstruction_data(ax_hist, ax_kde, ax_position, ax_stiffness, ax_reconstruct, trap_name, w_diss, color, p, t, trap_fns, midpoints, es_aligned, l_2_loss):
+  _1 = ax_hist.axvline(x = w_diss.mean(), color = color)
+  _2 = ax_hist.hist(w_diss, weights=jnp.ones(len(w_diss)) / len(w_diss), bins = 50, label = trap_name, alpha = 0.7, color = color)[-1]
+  
+  _3 = ax_kde.axvline(x = w_diss.mean(), color = color)
+  
+  _4 = ax_position.plot(t * p.param_set.dt / (10**(-6)), trap_fns[0](t), label = trap_name, color = color)[0]
+  _5 = ax_stiffness.plot(t * p.param_set.dt / (10**(-6)), trap_fns[1](t), label = trap_name, color = color)[0]
+  _6, _7 = plot_with_stddev(midpoints, es_aligned, label = f"{trap_name}; {l_2_loss:.2f}", ax = ax_reconstruct, color = color)
+  
+  return [_1,_2,_3,_4,_5,_6,_7]
 
 def plot_with_stddev(x,y, label=None, n=1, axis=0, ax=plt, dt=1., color = None):
   stddev = jnp.std(y, axis)
   mn = jnp.mean(y, axis)
-  ax.fill_between(x,
+  fill = ax.fill_between(x,
                   mn + n * stddev, mn - n * stddev, alpha=.3, color = color)
-  ax.plot(x, mn, label=label, color = color)
+  line = ax.plot(x, mn, label=label, color = color)[0]
+  return fill, line
   
 if __name__ == "__main__":
   args, p  = parse_args()
+  
+  plot_top = 5 # How many protocols to include?
   
   path = args.landscape_name.replace(" ", "_").replace(".", "_").lower()
   parent_dir = f"output_data/{path}/"
@@ -46,13 +60,13 @@ if __name__ == "__main__":
     "Work; Joint": ["joint","rev"],
     "Error; Position": ["position","rev"],
     "Error; Stiffness": ["stiffness","rev"],
-    # "Error; Joint": ["joint","rev"],
-    # "Split; Position": ["position","fwd", "split"],
-    # "Split; Stiffness": ["stiffness","fwd", "split"],
-    # "Split; Joint": ["joint","fwd", "split"],
-    # "Split; Position": ["position","rev", "split"],
-    # "Split; Stiffness": ["stiffness","rev", "split"],
-    # "Split; Joint": ["joint","rev", "split"],
+    "Error; Joint": ["joint","rev"],
+    "Split; Position": ["position","fwd", "split"],
+    "Split; Stiffness": ["stiffness","fwd", "split"],
+    "Split; Joint": ["joint","fwd", "split"],
+    "Split; Position": ["position","rev", "split"],
+    "Split; Stiffness": ["stiffness","rev", "split"],
+    "Split; Joint": ["joint","rev", "split"],
     "Near Equilibrium": [near_eq_file] if near_eq_file else None
     }
   
@@ -94,18 +108,23 @@ if __name__ == "__main__":
   ax_kde.set_xlabel("Dissipated Work (W)")
   ax_kde.set_ylabel("p(W)")
   kde_data = {}
-
-  no_trap_fn = p.param_set.energy_fn(no_trap = True)
+  plot_ranking = []
+  
+  no_trap_energy_fn = p.param_set.energy_fn(no_trap = True)
   time_vec = jnp.linspace(-20,20, 1000)
   if "triple" in args.landscape_name.lower():
     time_vec = jnp.linspace(-12,12, 1000)
-  ax_reconstruct.plot(time_vec, jnp.squeeze(no_trap_fn(time_vec.reshape(1,1,1000))) - no_trap_fn(p.r0_init), label = "Original", color = 'k')
+  ax_reconstruct.plot(time_vec, jnp.squeeze(no_trap_energy_fn(time_vec.reshape(1,1,1000))) - no_trap_energy_fn(p.r0_init), label = "Original", color = 'k')
 
   for ind, (trap_name, model_args) in enumerate(model_types.items()):
     if model_args is None:
       continue
     dir_name, coeff_files = find_coeff_file(model_args, args)
     trap_fns = make_trap_from_file(dir_name, coeff_files, psm, ssm, p)
+    
+    if trap_fns is None:
+      print(f"FAILURE: {trap_name} failed, continuing...")
+      continue
     
     t = jnp.arange(p.param_set.simulation_steps)
 
@@ -153,8 +172,9 @@ if __name__ == "__main__":
     landscape = (midpoints, energies)
     
     try:
-      disc = bcl.landscape_discrepancies(landscape, no_trap_fn, no_trap_fn(0.), -10., 10.)
+      disc = bcl.landscape_discrepancies(landscape, no_trap_energy_fn, no_trap_energy_fn(p.param_set.init_position_fwd), -10., 10.)
       bias = max(disc)
+      l_2_loss = jnp.mean(jnp.square(jnp.array(disc)))
     except:
       print(f"FAILURE: {trap_name} simulations failed. Continuing...")
       continue
@@ -169,16 +189,27 @@ if __name__ == "__main__":
     es_aligned = es - difference
     
     color = plot_color_list[ind]
-    ax_hist.axvline(x = w_diss.mean(), color = color)
-    ax_hist.hist(w_diss, weights=jnp.ones(len(w_diss)) / len(w_diss), bins = 50, label = trap_name, alpha = 0.7, color = color)
     kde_data[trap_name] = w_diss
-    ax_kde.axvline(x = w_diss.mean(), color = color)
     kde_colors.append(plot_color_list[ind])
-    ax_position.plot(t * p.param_set.dt / (10**(-6)), trap_fns[0](t), label = trap_name, color = color)
-    ax_stiffness.plot(t * p.param_set.dt / (10**(-6)), trap_fns[1](t), label = trap_name, color = color)
-    plot_with_stddev(midpoints, es_aligned, label = trap_name, ax = ax_reconstruct, color = color)
+    
+    line_data = plot_reconstruction_data(ax_hist, ax_kde, ax_position, ax_stiffness, ax_reconstruct, trap_name, w_diss, color, p, t, trap_fns, midpoints, es_aligned, l_2_loss)
+    plot_ranking.append({"trap_name": trap_name,  "loss" : l_2_loss, "plt_lines" : line_data, "kde_color_ind": len(kde_colors) - 1})
     #ax_reconstruct.plot(midpoints, energies_aligned, label = trap_name, color = color)
     print(f"Plotting complete for {trap_name}")
+  
+  plot_ranking.sort(key = lambda d: d["loss"], reverse = False)
+  plot_top = min(len(plot_ranking), plot_top)
+  to_remove = plot_ranking[plot_top:]
+  
+  to_remove.sort(key = lambda d: d["kde_color_ind"], reverse = True)
+  
+  for plot_dict in to_remove:
+    for line in plot_dict["plt_lines"]:
+      line.remove()
+    kde_data.pop(plot_dict["trap_name"])
+    kde_colors.pop(plot_dict["kde_color_ind"])
+    
+    
   sns.kdeplot(data = kde_data, palette = kde_colors, legend=True, ax = ax_kde)
   ax_hist.legend()
   ax_position.legend()
@@ -196,7 +227,7 @@ if __name__ == "__main__":
 # TODO: Table comparison of protocols --> Important graph to have! --> Adjust for different protocols etc
 
     # extensions = [-10,-5,0] # temporary
-    # disc_samples = bcl.landscape_discrepancies_samples(no_trap_rec_fn, no_trap_fn, extensions)
+    # disc_samples = bcl.landscape_discrepancies_samples(no_trap_rec_fn, no_trap_energy_fn, extensions)
     # disc_samples = jnp.array(disc_samples)
     # mean_disc_samples = disc_samples.mean()
     # bias_samples = disc_samples.max()
