b08e1139e88c24a166d6e4aeda32a7d911ac3334
RubberNoodles <ocheng@college.harvard.edu>
2025-02-02 10:34:16 -0500

    Make the plots look better. Option to include a training run.

--- figures/iterative_v2/plot_models.py
+++ figures/iterative_v2/plot_models.py
@@ -15,29 +15,40 @@ from argparse import ArgumentParser
 
 import jax.random as random
 import jax.numpy as jnp
+import numpy as np
 
 import barrier_crossing.plotting as plotting
 
 import seaborn as sns
 
-SEED = 1001
+SEED = 42
+BINS = 70
 results_dir = os.environ.get("ITERATIVE_RESULT_DIR", "results")
 MISC_SAVE_PATH = f"{results_dir}/misc_plots"
-BATCH_SIZE = 3000
-BINS = 70
-NUM_RECONSTRUCTIONS = 100
 
+testing_mode = False if os.environ.get("PLOT_TESTING", None) is None else True
+if testing_mode == True:
+    print("Testing mode is on. Plotting with reduced batch_size and num_reconstructions. `unset PLOT_TESTING` env variable if this was not intended.")
+    BATCH_SIZE = 100
+    NUM_RECONSTRUCTIONS = 10
+else:
+    BATCH_SIZE = 1000
+    NUM_RECONSTRUCTIONS = 100
 
 os.makedirs(MISC_SAVE_PATH, exist_ok = True)
 
 def load_data_from_result(file_dir):
     dfs = []
+    count = 0
     for file in os.listdir(file_dir):
         f = os.path.join(file_dir, file)
         if os.path.isfile(f):
             with open(f, "r") as g:
                 dfs.append(pd.read_csv(g.readline()))
-                
+        count +=1
+        if testing_mode == True:
+            if count == 5:
+                break
     return pd.concat(dfs)
 
 def load_model_from_coeff_file(param_set, coeff_file, name) -> bcm.ScheduleModel:
@@ -72,8 +83,8 @@ def make_df_names_nice(df):
     
     df["barrier_height_int"] = df["barrier_height"].apply(_str_to_float)
     
-def reconstruct_with_models(param_set, models: list[bcm.ScheduleModel], names: list[str], save_name) -> tuple[float, tuple]:
-    """Get bias from reconstructions."""
+def reconstruct_with_models(param_set: bcu.MDParameters, models: list[bcm.ScheduleModel], names: list[str], save_name) -> tuple[float, tuple]:
+    """Get bias and Free Energy from Batch Simulation + reconstructions."""
     
     key = random.PRNGKey(SEED)
 
@@ -83,9 +94,14 @@ def reconstruct_with_models(param_set, models: list[bcm.ScheduleModel], names: l
     reconstructed_landscapes = []
     reconstructed_labels = []
     biases = [] 
+    biases_std = []
     all_works = []
+    model_fes = []
+    model_fes_std = []
     for model, name in zip(models, names):
         energies = []
+        fes = []
+        midpoints = []
         for _ in tqdm(range(NUM_RECONSTRUCTIONS)):
             key, _ = random.split(key)
             
@@ -96,9 +112,9 @@ def reconstruct_with_models(param_set, models: list[bcm.ScheduleModel], names: l
                 pos_stiff_models = (model, param_set.k_s)
             
             no_key_simulate_fn = lambda key: param_set.simulate_fn(*pos_stiff_models, key = key, regime = "brownian")
-            _, (trajectories, works, _) = bcs.batch_simulate_harmonic(BATCH_SIZE, no_key_simulate_fn, key)
+            total_work, (trajectories, works, _) = bcs.batch_simulate_harmonic(BATCH_SIZE, no_key_simulate_fn, key)
             
-            midpoints, E = bcla.energy_reconstruction(
+            m, E = bcla.energy_reconstruction(
                             jnp.cumsum(works, axis=1), 
                             trajectories, 
                             BINS, 
@@ -107,14 +123,39 @@ def reconstruct_with_models(param_set, models: list[bcm.ScheduleModel], names: l
                             BATCH_SIZE, 
                             param_set.beta)
 
-            energies.append(E)
+            energies.append(bcla.interpolate_inf(E))
+            midpoints.append(m)
+            
+            fes.append(jnp.mean(jnp.exp(-param_set.beta * total_work)))
         
-        energies = bcla.interpolate_inf(jnp.mean(jnp.array(energies), axis = 0))
-        
-        ls = [midpoints, energies]
         first_well, second_well = bcu.get_trap_pos_from_params(param_set)
-        biases.append(jnp.max(bcla.landscape_discrepancies(ls, param_set.energy_fn(no_trap = True), first_well, first_well, second_well)))
         
+        all_biases = []
+        for m, e in zip(midpoints, energies):
+            all_biases.append(jnp.max(bcla.landscape_discrepancies(
+                (m, e), 
+                param_set.energy_fn(no_trap = True), 
+                r_min = first_well, 
+                r_max = second_well)))
+            
+            
+        # Average Landscape
+        ls = bcla.average_landscape(midpoints, energies)
+        
+        all_biases = jnp.array(all_biases)
+        bias = jnp.max(bcla.landscape_discrepancies(
+            ls, 
+            param_set.energy_fn(no_trap = True),
+            r_min = first_well,
+            r_max = second_well
+        )) 
+        
+        biases.append(float(bias))
+        biases_std.append(float(jnp.std(all_biases)))
+        
+        fes = jnp.array(fes)
+        model_fes.append(float(jnp.mean(fes)))
+        model_fes_std.append(float(jnp.std(fes)))
         reconstructed_landscapes.append(ls)
         reconstructed_labels.append(name)
         all_works.append(works)
@@ -130,8 +171,63 @@ def reconstruct_with_models(param_set, models: list[bcm.ScheduleModel], names: l
     plt.tight_layout()
     fig.savefig(MISC_SAVE_PATH + f"/{save_name}.png")
     
-    return [float(b) for b in biases]
+    return (biases, biases_std), (model_fes, model_fes_std)
 
+
+def plot_landscape_bias_with_increasing_barrier_height(landscape_df, zoom = False):
+    with open(landscape_df.params_file.iloc[0], "rb") as f:
+        param_set = pickle.load(f)
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.gca()
+    sns.lineplot(data = landscape_df, x = "barrier_height_int", y="bias", hue = "Model Name", errorbar=("sd", 1), markers="o", ax = ax)
+    if zoom == True:
+        ax.set_xlim([0, 17.5])
+        _b = landscape_df[landscape_df.barrier_height_int <= 17.5]["bias"]
+        ax.set_ylim([_b.mean() - 2 *_b.std(), _b.mean() + 2*_b.std()])
+        
+    plotting.add_energy_fn_plot(param_set, fig = fig)
+    ax.legend(loc="upper right")
+    ax.set_xlabel("Barrier Height (kT)")
+    ax.set_ylabel("Landscape Bias (kT)")
+    ax.grid()
+    
+    return fig
+
+
+def plot_dg_with_increasing_barrier_height(landscape_df): 
+    with open(landscape_df.params_file.iloc[0], "rb") as f:
+        param_set = pickle.load(f)
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.gca()
+    sns.lineplot(data = landscape_df, x = "barrier_height_int", y="dg", hue = "Model Name", markers="o", ax = ax)
+    plotting.add_energy_fn_plot(param_set, fig = fig)
+    ax.legend(loc="upper right")
+    ax.set_xlabel("Barrier Height (kT)")
+    ax.set_ylabel("Free Energy Difference (kT)")
+    ax.grid()
+    
+    return fig
+
+
+def resample_rows_for_plotting(df: pd.DataFrame, n_iter = 3000) -> pd.DataFrame:
+    """Each row, resample it with the given standard deviation `n_iter` number of times so plotting with seaborn is easy."""
+    new_rows = []
+    for _, row in df.iterrows():
+        resampling_cols = [{} for _ in range(n_iter)] 
+        new_row = row.to_dict()
+        for col in df.columns:
+            if col.endswith("_std"):
+                original_col = col[:-4]
+                resampled = np.random.normal(loc=row[original_col], scale = row[col], size = n_iter)
+                for id, r in enumerate(resampled):
+                    resampling_cols[id][original_col] = r
+                    
+        
+        for resampled_row in resampling_cols:
+            new_rows.append({**new_row, **resampled_row})
+    
+    return pd.DataFrame(new_rows)
+        
 
 if __name__ == "__main__":
     
@@ -143,8 +239,8 @@ if __name__ == "__main__":
     
     df = load_data_from_result(file_dir)
     df = df.reset_index()
-    reconstruct_results = []
-    
+    bias_results = [[], []]
+    dG_results = [[], []]
     for index, lb_df in df.groupby(["landscape_name", "barrier_height"]):
         with open(lb_df.params_file.iloc[0], "rb") as f:
             param_set = pickle.load(f)
@@ -158,24 +254,28 @@ if __name__ == "__main__":
             models.append(load_model_from_coeff_file(param_set, row["coeff_file"], name = row["model_name"]))
             names.append(row["model_name"])
         
-        biases = reconstruct_with_models(param_set, models, names, save_name = f"{full_name}kt")
-        reconstruct_results.append(pd.Series(data = biases, index = lb_df.index))
+        statistics = reconstruct_with_models(param_set, models, names, save_name = f"{full_name}kt")
+        (bias, bias_std), (dg, dg_std) = statistics
+        bias_results[0].append(pd.Series(data = bias, index = lb_df.index))
+        bias_results[1].append(pd.Series(data = bias_std, index = lb_df.index))
+        dG_results[0].append(pd.Series(data = dg, index = lb_df.index))
+        dG_results[1].append(pd.Series(data = dg_std, index = lb_df.index))
     
-    df["bias"] = pd.concat(reconstruct_results)
+    df["bias"] = pd.concat(bias_results[0])
+    df["bias_std"] = pd.concat(bias_results[1])
+    df["dg"] = pd.concat(dG_results[0])
+    df["dg_std"] = pd.concat(dG_results[1])
     
     make_df_names_nice(df)
+    resampled_df = resample_rows_for_plotting(df)
     
-    
-    for landscape_name, landscape_df in df.groupby("landscape_name"):
-        with open(landscape_df.params_file.iloc[0], "rb") as f:
-            param_set = pickle.load(f)
-        fig = plt.figure(figsize=(8,8))
-        ax = fig.gca()
-        sns.lineplot(data = landscape_df, x = "barrier_height_int", y="bias", hue = "Model Name", markers="o", ax = ax)
-        plotting.add_energy_fn_plot(param_set, fig = fig)
-        plt.tight_layout()
-        ax.set_xlabel("Barrier Height (kT)")
-        ax.set_ylabel("Landscape Bias (kT)")
-        fig.savefig(f"{results_dir}/{landscape_name}.png")
+    for landscape_name, landscape_df in resampled_df.groupby("landscape_name"):
+        landscape_bias_fig = plot_landscape_bias_with_increasing_barrier_height(landscape_df)
+        landscape_bias_fig.savefig(f"{results_dir}/{landscape_name}_bias_full.png")
         
+        landscape_bias_zoomed_fig = plot_landscape_bias_with_increasing_barrier_height(landscape_df, zoom = True)
+        landscape_bias_zoomed_fig.savefig(f"{results_dir}/{landscape_name}_bias_zoomed.png")
+        
+        # dg_fig = plot_dg_with_increasing_barrier_height(landscape_df)
+        # dg_fig.savefig(f"{results_dir}/{landscape_name}_dg.png")
         
--- figures/iterative_v2/slurm.sh
+++ figures/iterative_v2/slurm.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-while getopts "cl" flag; do
+while getopts "clt" flag; do
     case "$flag" in
         c) rm -rf results/*
         ;;
         l) rm -rf logs/*
+        ;;
+        t) train="true"
         ;;
         \?)
         echo "invalid argument"
@@ -16,18 +18,23 @@ source ~/.bashrc
 imp
 source ../env/bin/activate
 
-barrier_heights=('2.5' "10" "20" "40")
+# barrier_heights=("5" "10" "15" "20" "25" "30" "35" "40") # Jan 21
+# landscapes=("triple_well") # Jan 21
+# times=(0.00003 0.0001) # Jan 21
+# barrier_heights=('2.5' "10" "20" "40")
 # barrier_heights=('2.5' "10" "20" "30" "40")
-landscapes=("triple_well" "double_well" "asymmetric")
-# landscapes=("triple_well")
-times=(0.00003 0.0001  0.0003)
-# times=(0.00001 0.00003 0.00005 0.0001 0.0002 0.0004 0.0008)
+# barrier_heights=("5" "10" "15" "20" "25" "30" "35" "40") # Jan 22
+# landscapes=("double_well" "asymmetric") # Jan 22
+# times=(0.00003 0.0001) # Jan 22
+# barrier_heights=("5" "10" "15" "20" "25" "30" "35" "40") # Jan 23
+# landscapes=("triple_well" "double_well" "asymmetric") # Jan 23
+# times=(0.0003) # Jan 23
+barrier_heights=("5" "10" "15" "20" "25" "30" "35" "40") # Jan 23 v2
+landscapes=("triple_well" "double_well" "asymmetric") # Jan 23 v2
+times=(0.0003 0.0001 0.00003) # Jan 23 v2
 
-for t in "${times[@]}" 
-do
-    jobids=()
+iterate_over_params() {
 
-    export ITERATIVE_RESULT_DIR="results/$t"
 
     for landscape in "${landscapes[@]}"
     do
@@ -35,6 +42,7 @@ do
     do
 
             jobid=$(sbatch training.sbatch $landscape $barrier_height $t | awk '{print $NF}')
+            # jobid=""
 
             echo "SLURM $jobid running: python3 train.py --landscape_name $landscape --param_suffix $barrier_height --end_time $t"
             echo "sbatch training.sbatch $landscape $barrier_height $t"
@@ -44,18 +52,33 @@ do
     done
     done
 
-    jobid_string=""
+}
 
-    for jobid in "${jobids[@]}"
-    do
-        jobid_string+="$jobid,"
-    done
-    jobid_string="${jobid_string%?}"
-
+for t in "${times[@]}" 
+do  
+    jobids=()
+    export ITERATIVE_RESULT_DIR="results/$t"
     file_dir="$ITERATIVE_RESULT_DIR/files"
 
-    echo "sbatch -d afterok:$jobid_string -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir"
-    sbatch -d "afterok:$jobid_string" -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir
+    if [[ "$train" == "true" ]]; then
+        iterate_over_params
+        jobid_string=""
+        for jobid in "${jobids[@]}"
+        do
+            jobid_string+="$jobid,"
+        done
+        jobid_string="${jobid_string%?}"
+
+        echo "sbatch -d afterok:$jobid_string -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir"
+        sbatch -d "afterok:$jobid_string" -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir
+        # sbatch plotting.sbatch $file_dir
+    else
+        echo "sbatch -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir"
+        sbatch -t 0-08:00 --mem-per-cpu=16000 plotting.sbatch $file_dir
+    fi
+
+
+    
 done
 echo "All slurm commands run, exiting."
 # Responsibiltiy of slurm to wait for all train to finish before running the reconstructions.
