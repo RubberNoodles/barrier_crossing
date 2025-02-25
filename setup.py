1be62313d0a143b2a7b281dad09ef7ebd0f37e77
RubberNoodles <ocheng@college.harvard.edu>
2025-02-25 13:15:38 -0500

    Add required dependencies.

--- setup.py
+++ setup.py
@@ -4,7 +4,19 @@ setup(
     name="barrier_crossing",
     version="0.1.0",
     packages=["barrier_crossing"],
-    install_requires=[],  # Add dependencies if needed
+    install_requires=[ "absl-py==1.1.0",
+    "jax==0.4.16",
+    "jax-md==0.2.8",
+    "matplotlib==3.5.2",
+    "numpy==1.22.4",
+    "optax==0.1.7",
+    "pandas==1.4.3",
+    "scipy==1.7.3",
+    "seaborn==0.13.2",
+    "setuptools==65.6.3",  # Keeping the latest version
+    "tensorflow-probability==0.22.0",
+    "tqdm==4.64.1" 
+    ],
     author="Oliver Cheng, Zosia Adamska, Megan Engel",
     description="Iteratively Reconstructing Using Nonequilibrium Pulling.",
     url="https://github.com/rubbernoodles/barrier_crossing",  # Update with your repo URL
